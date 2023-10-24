428e7dacaca6bdae6978df6afea89551baab0d16
mvibert88 <macvibert@gmail.com>
2023-10-24 13:21:44 -0700

    like whole amplify backend setup and connection and stuff

---
+++ app/src/ui-components/UserUpdateForm.d.ts
@@ -0,0 +1,52 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, SelectFieldProps, TextFieldProps } from "@aws-amplify/ui-react";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type UserUpdateFormInputValues = {
+    email?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+    dateJoined?: string;
+    userType?: string;
+};
+export declare type UserUpdateFormValidationValues = {
+    email?: ValidationFunction<string>;
+    password?: ValidationFunction<string>;
+    firstName?: ValidationFunction<string>;
+    lastName?: ValidationFunction<string>;
+    dateJoined?: ValidationFunction<string>;
+    userType?: ValidationFunction<string>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type UserUpdateFormOverridesProps = {
+    UserUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    email?: PrimitiveOverrideProps<TextFieldProps>;
+    password?: PrimitiveOverrideProps<TextFieldProps>;
+    firstName?: PrimitiveOverrideProps<TextFieldProps>;
+    lastName?: PrimitiveOverrideProps<TextFieldProps>;
+    dateJoined?: PrimitiveOverrideProps<TextFieldProps>;
+    userType?: PrimitiveOverrideProps<SelectFieldProps>;
+} & EscapeHatchProps;
+export declare type UserUpdateFormProps = React.PropsWithChildren<{
+    overrides?: UserUpdateFormOverridesProps | undefined | null;
+} & {
+    id?: string;
+    user?: any;
+    onSubmit?: (fields: UserUpdateFormInputValues) => UserUpdateFormInputValues;
+    onSuccess?: (fields: UserUpdateFormInputValues) => void;
+    onError?: (fields: UserUpdateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: UserUpdateFormInputValues) => UserUpdateFormInputValues;
+    onValidate?: UserUpdateFormValidationValues;
+} & React.CSSProperties>;
+export default function UserUpdateForm(props: UserUpdateFormProps): React.ReactElement;
---
+++ app/src/ui-components/UserUpdateForm.jsx
@@ -0,0 +1,385 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import {
+  Button,
+  Flex,
+  Grid,
+  SelectField,
+  TextField,
+} from "@aws-amplify/ui-react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { fetchByPath, validateField } from "./utils";
+import { API } from "aws-amplify";
+import { getUser } from "../graphql/queries";
+import { updateUser } from "../graphql/mutations";
+export default function UserUpdateForm(props) {
+  const {
+    id: idProp,
+    user: userModelProp,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    dateJoined: "",
+    userType: "",
+  };
+  const [email, setEmail] = React.useState(initialValues.email);
+  const [password, setPassword] = React.useState(initialValues.password);
+  const [firstName, setFirstName] = React.useState(initialValues.firstName);
+  const [lastName, setLastName] = React.useState(initialValues.lastName);
+  const [dateJoined, setDateJoined] = React.useState(initialValues.dateJoined);
+  const [userType, setUserType] = React.useState(initialValues.userType);
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    const cleanValues = userRecord
+      ? { ...initialValues, ...userRecord }
+      : initialValues;
+    setEmail(cleanValues.email);
+    setPassword(cleanValues.password);
+    setFirstName(cleanValues.firstName);
+    setLastName(cleanValues.lastName);
+    setDateJoined(cleanValues.dateJoined);
+    setUserType(cleanValues.userType);
+    setErrors({});
+  };
+  const [userRecord, setUserRecord] = React.useState(userModelProp);
+  React.useEffect(() => {
+    const queryData = async () => {
+      const record = idProp
+        ? (
+            await API.graphql({
+              query: getUser.replaceAll("__typename", ""),
+              variables: { id: idProp },
+            })
+          )?.data?.getUser
+        : userModelProp;
+      setUserRecord(record);
+    };
+    queryData();
+  }, [idProp, userModelProp]);
+  React.useEffect(resetStateValues, [userRecord]);
+  const validations = {
+    email: [{ type: "Required" }, { type: "Email" }],
+    password: [{ type: "Required" }],
+    firstName: [],
+    lastName: [],
+    dateJoined: [],
+    userType: [],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          email,
+          password,
+          firstName: firstName ?? null,
+          lastName: lastName ?? null,
+          dateJoined: dateJoined ?? null,
+          userType: userType ?? null,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value === "") {
+              modelFields[key] = null;
+            }
+          });
+          await API.graphql({
+            query: updateUser.replaceAll("__typename", ""),
+            variables: {
+              input: {
+                id: userRecord.id,
+                ...modelFields,
+              },
+            },
+          });
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+        } catch (err) {
+          if (onError) {
+            const messages = err.errors.map((e) => e.message).join("\n");
+            onError(modelFields, messages);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "UserUpdateForm")}
+      {...rest}
+    >
+      <TextField
+        label="Email"
+        isRequired={true}
+        isReadOnly={false}
+        value={email}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email: value,
+              password,
+              firstName,
+              lastName,
+              dateJoined,
+              userType,
+            };
+            const result = onChange(modelFields);
+            value = result?.email ?? value;
+          }
+          if (errors.email?.hasError) {
+            runValidationTasks("email", value);
+          }
+          setEmail(value);
+        }}
+        onBlur={() => runValidationTasks("email", email)}
+        errorMessage={errors.email?.errorMessage}
+        hasError={errors.email?.hasError}
+        {...getOverrideProps(overrides, "email")}
+      ></TextField>
+      <TextField
+        label="Password"
+        isRequired={true}
+        isReadOnly={false}
+        value={password}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              password: value,
+              firstName,
+              lastName,
+              dateJoined,
+              userType,
+            };
+            const result = onChange(modelFields);
+            value = result?.password ?? value;
+          }
+          if (errors.password?.hasError) {
+            runValidationTasks("password", value);
+          }
+          setPassword(value);
+        }}
+        onBlur={() => runValidationTasks("password", password)}
+        errorMessage={errors.password?.errorMessage}
+        hasError={errors.password?.hasError}
+        {...getOverrideProps(overrides, "password")}
+      ></TextField>
+      <TextField
+        label="First name"
+        isRequired={false}
+        isReadOnly={false}
+        value={firstName}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              password,
+              firstName: value,
+              lastName,
+              dateJoined,
+              userType,
+            };
+            const result = onChange(modelFields);
+            value = result?.firstName ?? value;
+          }
+          if (errors.firstName?.hasError) {
+            runValidationTasks("firstName", value);
+          }
+          setFirstName(value);
+        }}
+        onBlur={() => runValidationTasks("firstName", firstName)}
+        errorMessage={errors.firstName?.errorMessage}
+        hasError={errors.firstName?.hasError}
+        {...getOverrideProps(overrides, "firstName")}
+      ></TextField>
+      <TextField
+        label="Last name"
+        isRequired={false}
+        isReadOnly={false}
+        value={lastName}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              password,
+              firstName,
+              lastName: value,
+              dateJoined,
+              userType,
+            };
+            const result = onChange(modelFields);
+            value = result?.lastName ?? value;
+          }
+          if (errors.lastName?.hasError) {
+            runValidationTasks("lastName", value);
+          }
+          setLastName(value);
+        }}
+        onBlur={() => runValidationTasks("lastName", lastName)}
+        errorMessage={errors.lastName?.errorMessage}
+        hasError={errors.lastName?.hasError}
+        {...getOverrideProps(overrides, "lastName")}
+      ></TextField>
+      <TextField
+        label="Date joined"
+        isRequired={false}
+        isReadOnly={false}
+        type="date"
+        value={dateJoined}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              password,
+              firstName,
+              lastName,
+              dateJoined: value,
+              userType,
+            };
+            const result = onChange(modelFields);
+            value = result?.dateJoined ?? value;
+          }
+          if (errors.dateJoined?.hasError) {
+            runValidationTasks("dateJoined", value);
+          }
+          setDateJoined(value);
+        }}
+        onBlur={() => runValidationTasks("dateJoined", dateJoined)}
+        errorMessage={errors.dateJoined?.errorMessage}
+        hasError={errors.dateJoined?.hasError}
+        {...getOverrideProps(overrides, "dateJoined")}
+      ></TextField>
+      <SelectField
+        label="User type"
+        placeholder="Please select an option"
+        isDisabled={false}
+        value={userType}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              password,
+              firstName,
+              lastName,
+              dateJoined,
+              userType: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.userType ?? value;
+          }
+          if (errors.userType?.hasError) {
+            runValidationTasks("userType", value);
+          }
+          setUserType(value);
+        }}
+        onBlur={() => runValidationTasks("userType", userType)}
+        errorMessage={errors.userType?.errorMessage}
+        hasError={errors.userType?.hasError}
+        {...getOverrideProps(overrides, "userType")}
+      >
+        <option
+          children="Patient"
+          value="patient"
+          {...getOverrideProps(overrides, "userTypeoption0")}
+        ></option>
+        <option
+          children="Doctor"
+          value="doctor"
+          {...getOverrideProps(overrides, "userTypeoption1")}
+        ></option>
+      </SelectField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Reset"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          isDisabled={!(idProp || userModelProp)}
+          {...getOverrideProps(overrides, "ResetButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={
+              !(idProp || userModelProp) ||
+              Object.values(errors).some((e) => e?.hasError)
+            }
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
