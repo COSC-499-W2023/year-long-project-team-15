3e1267cfed01cfb76877d8cb443266506cb7d779
mvibert88 <macvibert@gmail.com>
2023-10-24 14:20:26 -0700

    file fixes

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import UserCreateForm from './ui-components/UserCreateForm';
 
--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify } from 'aws-amplify'
-import config from './aws-exports'
-import '@aws-amplify/ui-react/styles.css'
+import { Amplify } from 'aws-amplify';
+import awsmobile from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
 
 
-Amplify.configure(config)
+Amplify.configure(awsmobile)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
