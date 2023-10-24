cdee4fd7e703049420054a831bc74f80f4dbca77
mvibert88 <macvibert@gmail.com>
2023-10-24 14:06:48 -0700

    studio style fix

--- app/src/index.js
+++ app/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+
+
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <App />
+  )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
