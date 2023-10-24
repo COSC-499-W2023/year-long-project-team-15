165ed07ae72098366c58c4832b20e31238bbbd70
mvibert88 <macvibert@gmail.com>
2023-10-24 13:27:39 -0700

    form render fix

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import UserCreateForm from './ui-components/UserCreateForm';
+import UserCreateForm from './ui-components/UserCreateForm.jsx';
 
 function App() {
   return (
