92b1fc0b403e027903902212f6c53edd91701e51
mvibert88 <macvibert@gmail.com>
2023-10-26 16:33:35 -0700

    cognito lambda setup

---
+++ capstone/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,32 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blurvidauth": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "blurvidauth5c3350285c335028": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminGroupRole": "string"
+    }
+  },
+  "function": {
+    "newUserToDB": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ capstone/src/graphql/mutations.js
@@ -0,0 +1,504 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      email
+      password
+      firstName
+      lastName
+      dateJoined
+      userType
+      sentFriendRequests {
+        nextToken
+        __typename
+      }
+      receivedFriendRequests {
+        nextToken
+        __typename
+      }
+      uploadedVideos {
+        nextToken
+        __typename
+      }
+      sentVideoMessages {
+        nextToken
+        __typename
+      }
+      receivedVideoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      email
+      password
+      firstName
+      lastName
+      dateJoined
+      userType
+      sentFriendRequests {
+        nextToken
+        __typename
+      }
+      receivedFriendRequests {
+        nextToken
+        __typename
+      }
+      uploadedVideos {
+        nextToken
+        __typename
+      }
+      sentVideoMessages {
+        nextToken
+        __typename
+      }
+      receivedVideoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      email
+      password
+      firstName
+      lastName
+      dateJoined
+      userType
+      sentFriendRequests {
+        nextToken
+        __typename
+      }
+      receivedFriendRequests {
+        nextToken
+        __typename
+      }
+      uploadedVideos {
+        nextToken
+        __typename
+      }
+      sentVideoMessages {
+        nextToken
+        __typename
+      }
+      receivedVideoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createFriendRequest = /* GraphQL */ `
+  mutation CreateFriendRequest(
+    $input: CreateFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    createFriendRequest(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateFriendRequest = /* GraphQL */ `
+  mutation UpdateFriendRequest(
+    $input: UpdateFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    updateFriendRequest(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteFriendRequest = /* GraphQL */ `
+  mutation DeleteFriendRequest(
+    $input: DeleteFriendRequestInput!
+    $condition: ModelFriendRequestConditionInput
+  ) {
+    deleteFriendRequest(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createVideo = /* GraphQL */ `
+  mutation CreateVideo(
+    $input: CreateVideoInput!
+    $condition: ModelVideoConditionInput
+  ) {
+    createVideo(input: $input, condition: $condition) {
+      id
+      uploadDate
+      videoKey
+      title
+      description
+      uploader {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      uploaderID
+      videoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateVideo = /* GraphQL */ `
+  mutation UpdateVideo(
+    $input: UpdateVideoInput!
+    $condition: ModelVideoConditionInput
+  ) {
+    updateVideo(input: $input, condition: $condition) {
+      id
+      uploadDate
+      videoKey
+      title
+      description
+      uploader {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      uploaderID
+      videoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteVideo = /* GraphQL */ `
+  mutation DeleteVideo(
+    $input: DeleteVideoInput!
+    $condition: ModelVideoConditionInput
+  ) {
+    deleteVideo(input: $input, condition: $condition) {
+      id
+      uploadDate
+      videoKey
+      title
+      description
+      uploader {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      uploaderID
+      videoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createVideoMessage = /* GraphQL */ `
+  mutation CreateVideoMessage(
+    $input: CreateVideoMessageInput!
+    $condition: ModelVideoMessageConditionInput
+  ) {
+    createVideoMessage(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      video {
+        id
+        uploadDate
+        videoKey
+        title
+        description
+        uploaderID
+        createdAt
+        updatedAt
+        __typename
+      }
+      videoID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateVideoMessage = /* GraphQL */ `
+  mutation UpdateVideoMessage(
+    $input: UpdateVideoMessageInput!
+    $condition: ModelVideoMessageConditionInput
+  ) {
+    updateVideoMessage(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      video {
+        id
+        uploadDate
+        videoKey
+        title
+        description
+        uploaderID
+        createdAt
+        updatedAt
+        __typename
+      }
+      videoID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteVideoMessage = /* GraphQL */ `
+  mutation DeleteVideoMessage(
+    $input: DeleteVideoMessageInput!
+    $condition: ModelVideoMessageConditionInput
+  ) {
+    deleteVideoMessage(input: $input, condition: $condition) {
+      id
+      date
+      status
+      sender {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      senderID
+      receiver {
+        id
+        email
+        password
+        firstName
+        lastName
+        dateJoined
+        userType
+        createdAt
+        updatedAt
+        __typename
+      }
+      receiverID
+      video {
+        id
+        uploadDate
+        videoKey
+        title
+        description
+        uploaderID
+        createdAt
+        updatedAt
+        __typename
+      }
+      videoID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
