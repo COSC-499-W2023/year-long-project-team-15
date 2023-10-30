468ee9410a4023d4d9a28d3a9d36eaeae27cd7ea
Willg0115 <willg0115@outlook.com>
2023-10-29 21:49:03 -0700

    figured out import problems

--- app/amplify/backend/function/BlurVidPreSignup/src/custom.js
+++ app/amplify/backend/function/BlurVidPreSignup/src/custom.js
@@ -2,50 +2,13 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-//import createUser from "../../../../../src/graphql/mutations.js";
+const createUser = require("../../../../../src/graphql/mutations.js");
 const fetch = require("node-fetch");
 
 exports.handler = async(event, context) => {
   const GRAPHQL_ENDPOINT = process.env.API_BLURVID_GRAPHQLAPIENDPOINTOUTPUT;
   const GRAPHQL_API_KEY = process.env.API_BLURVID_GRAPHQLAPIKEYOUTPUT;
   
-  const createUser = /* GraphQL */ `
-  mutation CreateUser(
-    $input: CreateUserInput!
-  ) {
-    createUser(input: $input) {
-      id
-      email
-      password
-      firstName
-      lastName
-      dateJoined
-      sentFriendRequests {
-        nextToken
-        __typename
-      }
-      receivedFriendRequests {
-        nextToken
-        __typename
-      }
-      uploadedVideos {
-        nextToken
-        __typename
-      }
-      sentVideoMessages {
-        nextToken
-        __typename
-      }
-      receivedVideoMessages {
-        nextToken
-        __typename
-      }
-      createdAt
-      updatedAt
-      __typename
-    }
-  }
-`;
   let date = new Date();
 
   const variables = {
