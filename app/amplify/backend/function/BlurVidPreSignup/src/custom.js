b82f4e3a8752930a19d1db6588f26c51a8895b5e
Willg0115 <willg0115@outlook.com>
2023-10-29 22:04:46 -0700

    imporsts still broke, going back. event.json updated

--- app/amplify/backend/function/BlurVidPreSignup/src/custom.js
+++ app/amplify/backend/function/BlurVidPreSignup/src/custom.js
@@ -2,13 +2,50 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-const createUser = require("../../../../../src/graphql/mutations.js");
 const fetch = require("node-fetch");
 
 exports.handler = async(event, context) => {
   const GRAPHQL_ENDPOINT = process.env.API_BLURVID_GRAPHQLAPIENDPOINTOUTPUT;
   const GRAPHQL_API_KEY = process.env.API_BLURVID_GRAPHQLAPIKEYOUTPUT;
   
+  const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      email
+      password
+      firstName
+      lastName
+      dateJoined
+      sentFriendRequests {
+        nextToken
+        __typename
+      }
+      receivedFriendRequests {
+        nextToken
+        __typename
+      }
+      uploadedVideos {
+        nextToken
+        __typename
+      }
+      sentVideoMessages {
+        nextToken
+        __typename
+      }
+      receivedVideoMessages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
   let date = new Date();
 
   const variables = {
