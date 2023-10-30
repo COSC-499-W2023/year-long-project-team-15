50825ddd26d62907d7215a2a7420600bea9b82bc
Willg0115 <willg0115@outlook.com>
2023-10-29 21:04:29 -0700

    trying more things

--- app/src/App.js
+++ app/src/App.js
@@ -2,26 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import "@aws-amplify/ui-react/styles.css";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AmplifySignOut />
+      <h1>Welcome to BlurVid!</h1>
+      
     </div>
   );
 }
