7e6cf87e7224b68b315eb3fa870fd6c3d24b9c3a
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2023-11-01 12:31:08 -0700

    added simple view for user info

--- app/amp-dev/src/App.js
+++ app/amp-dev/src/App.js
@@ -1,13 +1,31 @@
+import {Amplify} from 'aws-amplify';
+import awsExports from './aws-exports';
+import {useState, useEffect} from 'react';
+
 import "@aws-amplify/ui-react/styles.css" 
-import {withAuthenticator} from '@aws-amplify/ui-react';
+import {withAuthenticator, View, Text, Flex} from '@aws-amplify/ui-react';
+Amplify.configure(awsExports); 
 
 function App({user, signOut}) {
     return (
-      <>
-        <h1>Hello {user.attributes.name}, </h1>
-        <button onClick={signOut}>Sign out</button>
-      </>
+      <Flex direction="column" alignItems="center">
+      {user ? (
+        <View width="300px" padding="1rem" shadow="md" border="1px solid" borderColor="gray.200">
+          <Text fontSize="xl" fontWeight="bold" marginBottom="1rem">
+            User Profile
+          </Text>
+          <Text>
+            <strong>Name:</strong> {user.attributes.name}
+          </Text>
+          <Text>
+            <strong>Email:</strong> {user.attributes.email}
+          </Text>
+        </View>
+      ) : (
+        <Text fontSize="lg">Please sign in to view your profile.</Text>
+      )}
+    </Flex>
     );
-  }
+  };
 
 export default withAuthenticator(App);
