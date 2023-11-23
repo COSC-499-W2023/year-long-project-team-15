513c953eb5b2aebf5c77b31b748fb209529f16cf
Willg0115 <willg0115@outlook.com>
2023-11-23 11:36:50 -0800

    non-functional header and sidebar updated

--- app/src/ui-components/Header.js
+++ app/src/ui-components/Header.js
@@ -1,43 +1,29 @@
 import * as React from "react"; 
-
-// importing material UI components 
-import AppBar from "@mui/material/AppBar"; 
-import Box from "@mui/material/Box"; 
-import Toolbar from "@mui/material/Toolbar"; 
-import Typography from "@mui/material/Typography"; 
-import Button from "@mui/material/Button"; 
-import IconButton from "@mui/material/IconButton"; 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { fontWeight } from "@mui/system";
+
 
 export default function Header() { 
 return ( 
-	<AppBar position="static"> 
-		<Toolbar> 
-		{/*Inside the IconButton, we 
-		can render various icons*/} 
-		<IconButton 
-			size="large"
-			edge="start"
-			color="inherit"
-			aria-label="menu"
-			sx={{ mr: 2 }} 
-		> 
-			{/*This is a simple Menu 
-			Icon wrapped in Icon */} 
-			<AccountCircleIcon /> 
-		</IconButton> 
-		{/* The Typography component applies 
-		default font weights and sizes */} 
 
-		<Typography variant="h6"
-			component="div" sx={{ flexGrow: 1 }}> 
-			Users Name 
-		</Typography> 
-        <Typography variant="h5" align="right" fontWeight="bold"
-			component="div" sx={{ flexGrow: 1 }}> 
-			BlurVid 
-		</Typography>
-		</Toolbar> 
-	</AppBar> 
+<nav class="navbar navbar-expand-lg bg-dark" >
+  <div class="container-fluid" >
+    <div class="collapse navbar-collapse" id="navbarText" style={{color: "white"}}>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+        <li class="nav-item">
+            <button type="button" class="btn">
+                <AccountCircleIcon style={{color: "white"}}/> 
+            </button>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" aria-disabled="true" style={{color: "white"}}>Users Name</a>
+        </li>
+      </ul>
+      <h3 style={{fontWeight:"bold"}}>
+        BlurVid
+      </h3>
+    </div>
+  </div>
+</nav>
 ); 
 }
