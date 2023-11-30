c510a02d2488ee8a2e99c5bd4d897f20712e016c
sakshamrana7 <rana.saksham10@gmail.com>
2023-11-30 05:46:20 -0800

    EditProfile done, need to test

--- amp-dev/src/EditProfile.js
+++ amp-dev/src/EditProfile.js
@@ -1,43 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import { Auth, API } from 'aws-amplify';
-import * as queries from './graphql/mutations';
+import { API, graphqlOperation } from 'aws-amplify';
+import { updateUser } from './graphql/mutations'; // Import the updateUser mutation
 
 function EditProfileForm({ user, onUpdate }) {
+  const [id, setId] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
+    setId(user.attributes.sub || ''); // Populate with user's ID
     setName(user.attributes.name || ''); // Populate with user's name
     setEmail(user.attributes.email || ''); // Populate with user's email
   }, [user]);
 
+  const validateForm = () => {
+    const newErrors = {};
+    if (!name.trim()) newErrors.name = 'Name is required';
+    if (!email.trim()) {
+      newErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = 'Email address is invalid';
+    }
+    return newErrors;
+  };
+
   const handleUpdateProfile = async () => {
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+      return;
+    }
+
     try {
-      // Call GraphQL mutation to update user profile in DynamoDB
-      const response = await API.graphql({
-        query: queries.updateUser,
-        variables: {
-          input: {
-            id: user.attributes.sub,
-            name,
-            email,
-          },
-        },
-      });
+      // Prepare the input for the updateUser mutation
+      const input = {
+        id, // Assuming 'id' is the unique identifier for the user
+        name, // New name from the form input
+        email, // New email from the form input
+      };
 
-      // Handle the response as needed
+      // Caling the updateUser mutation
+      const response = await API.graphql(graphqlOperation(updateUser, { input }));
       console.log('User profile updated:', response);
-
-      // Update local state or trigger a callback to refresh user data
-      onUpdate();
+      onUpdate(); // Callback to parent component to reflect changes
     } catch (error) {
       console.error('Error updating user profile:', error);
-      // Handle error, show a message to the user, etc.
+      // sets error states here to display in the UI
     }
   };
 
   return (
     <div>
+      <label>
+        ID:
+        <input
+          type="text"
+          value={id}
+          readOnly // ID field is read-only
+        />
+      </label>
+      <br />
       <label>
         Name:
         <input
@@ -45,6 +68,7 @@ function EditProfileForm({ user, onUpdate }) {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
       </label>
       <br />
       <label>
@@ -54,12 +78,14 @@ function EditProfileForm({ user, onUpdate }) {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
       </label>
       <br />
       <button onClick={handleUpdateProfile}>Update Profile</button>
     </div>
   );
 }
-
 export default EditProfileForm;
+
+
 
