1c207ee91889ff5a1434f10f8e06f4a7de567b5b
Willg0115 <willg0115@outlook.com>
2023-11-29 22:10:23 -0800

    routing set up a pages folder added, some refactoring as needed

--- amp-dev/src/App.js
+++ amp-dev/src/App.js
@@ -1,27 +1,19 @@
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-import { useState } from 'react';
 import "@aws-amplify/ui-react/styles.css";
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import Sidebar from './containers/Sidebar';
-import ChatView from './containers/chatView';
-import FriendContext from './context/FriendContext';
-import Header from './containers/Header';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Account from './pages/Account'
 Amplify.configure(awsExports);
 
 function App() {
-const [selectedFriend, setSelectedFriend] = useState(null);
   return (
-    <FriendContext.Provider value={{ selectedFriend, setSelectedFriend }}>
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-            <Header />
-            <div style={{ display: 'flex', flex: 1 }}>
-                <Sidebar />
-                <ChatView />
-            </div>
-        </div>  
-    </FriendContext.Provider>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/accountpage" element={<Account />} />
+    </Routes>
   );
-};
+}
 
 export default withAuthenticator(App);
--- amp-dev/src/containers/Header.js
+++ amp-dev/src/containers/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Auth from '@aws-amplify/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [name, setName] = useState('');
@@ -19,12 +20,22 @@ const Header = () => {
     fetchUserData();
   }, []);
 
+    const navigate = useNavigate();
+    
+    const handleAccountClick = () => {
+      navigate('/accountpage'); // Navigate to the Account page
+    };
+  
+    const handleMainClick = () => {
+      navigate('/'); // Navigate to the new page
+    };
+
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
         <div className="collapse navbar-collapse" id="navbarText" style={{ color: "white" }}>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick={handleAccountClick}>
                 <AccountCircleIcon style={{ color: "white" }} />
               </button>
             </li>
@@ -33,7 +44,7 @@ const Header = () => {
             </li>
           </ul>
           <h3 style={{ fontWeight: "bold", paddingRight: "1em"}}>
-            <button type="button">BlurVid</button>
+            <button type="button" onClick={handleMainClick} style={{border: "none"}}>BlurVid</button>
           </h3>
         </div>
     </nav>
