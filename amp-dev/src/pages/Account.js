5d921f0cecb68d4e3228157bfa8c9d763640a58a
kyle-keim <kyle.keim777@gmail.com>
2023-12-03 17:39:04 -0800

    addition of pending friend requests

--- amp-dev/src/pages/Account.js
+++ amp-dev/src/pages/Account.js
@@ -1,11 +1,26 @@
 import Header from "../containers/Header";
+import AccountSidebar from '../containers/AccountSidebar';
+import FriendContext from '../context/FriendContext';
+import { useState } from 'react';
+
 
 function Account() {
+    const [selectedFriend, setSelectedFriend] = useState(null);
     return(
-        <>
-            <Header />
-            Account Page
-        </>
+        
+        <FriendContext.Provider value={{ selectedFriend, setSelectedFriend }}>
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+                <Header />
+                <div style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
+                    <div style={{ flex: 3 }}>
+                        <AccountSidebar />
+                    </div>
+                    <div style={{ flex: 2, paddingRight: '500px' }}>
+                        <h1>Account Page</h1> 
+                    </div>
+                </div>
+            </div>
+        </FriendContext.Provider>         
     );
 };
 
