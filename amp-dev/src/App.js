ba0aab6e92a34c4473c32cd765b417dd0f26e8c9
Willg0115 <willg0115@outlook.com>
2024-01-21 17:00:15 -0500

    Revert "Revert "Revert "week 2 T2 Will's log done"""
    This reverts commit a6c9eb409f557d8b25bdd5e44ba626c9641263ee.

---
+++ amp-dev/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Account from './pages/Account'
+Amplify.configure(awsExports);
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/accountpage" element={<Account />} />
+    </Routes>
+  );
+}
+
+export default withAuthenticator(App);
