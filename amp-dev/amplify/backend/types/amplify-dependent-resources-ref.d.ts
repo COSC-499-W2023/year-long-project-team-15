07186c612a7a3101c2cfb4f1c71e37a80a6b6a93
mvibert88 <macvibert@gmail.com>
2024-01-29 17:45:52 -0800

    compilation fix

---
+++ amp-dev/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "yearlongprojectteam15appampdev": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "yearlongprojectteam15appampdev": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "yearlongprojectteam15appampdevPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "blurvidContent": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
